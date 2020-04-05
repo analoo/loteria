1b1dbc8d36f591d80b19874eaafd39478c41b355
Jonathon Mah <me@JonathonMah.com>
2020-04-04 17:28:33 -0700

    Construct card in board.js

--- js/boards.js
+++ js/boards.js
@@ -9,13 +9,15 @@ var tabla1 = {
     displayName: "Tabla 1",
     images: [cards[0], cards[1]],
     won: false
-}
+};
 
 
 function displayBoard(obj){
-    $("#card0").attr("data-src", cards[0].location);
-    $("#card0").text(cards[0].name);
-
+    $("#card0").append($("<img>").attr("data-src", cards[0].location)
+                                 .attr("width", 150)
+                                 .attr("height", 400)
+                                 .attr("uk-img", ""));
+    $("#card0").append("<p>" + cards[0].name +"</p>");
 }
 
 displayBoard(tabla1)
