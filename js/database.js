cf53f8e3faf18190790c44d69f60f59827522948
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-04 18:18:47 -0700

    connected firebase database and updated script links accordingly

---
+++ js/database.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC44gu7DL9WF-R7m8hKWC0JbpkmkLvj1p8",
+    authDomain: "loteria-29611.firebaseapp.com",
+    databaseURL: "https://loteria-29611.firebaseio.com",
+    projectId: "loteria-29611",
+    storageBucket: "loteria-29611.appspot.com",
+    messagingSenderId: "804338960169",
+    appId: "1:804338960169:web:c2bee9425ba46195ab5938"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
